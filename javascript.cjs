38c11a6c668d6ed9151d148ca53cbccd65db667b
General-Cho <124175990+General-Cho@users.noreply.github.com>
2023-02-04 17:29:36 -0600

    Add files via upload

---
+++ javascript.cjs
@@ -0,0 +1,16 @@
+// Identifying our image
+
+
+const GenCho = document.querySelector("#GenCho");
+
+
+// Adding reaction when image is clicked
+
+
+GenCho.addEventListener("click", () => {
+
+
+    alert(`do not clik mi or yu ar goin to prizin`);
+
+
+});
