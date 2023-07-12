b1f5f145af784980a08b3f936d73d3538b7210da
General Cho <124175990+General-Cho@users.noreply.github.com>
2023-07-12 08:05:09 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,5 +3,5 @@ const GenCho = document.querySelector("#GenCho");
 
 // Adding reaction when image is clicked
 GenCho.addEventListener("click", () => {
-    alert(`Cho Says: yoo hav entured me website me will gets yoo.`);
+    alert(`Cho Says: yoo hav enturd me web, give me some chimkin broth or else.....`);
 });
